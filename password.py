757dd69037290b6f4491501977d51d47f96db39a
pranjalmourya <pranjalmaurya278@gmail.com>
2025-12-21 14:27:25 +0530

    List of project what i did

---
+++ password.py
@@ -0,0 +1,13 @@
+'''User is given 3 chances to enter correct password.
+ If more than 3 than tell the user that account blocked'''
+ # example
+attempts = 0
+while True:
+    pwd = input("Enter password: ")
+    attempts += 1
+    if pwd == "admin123":
+        print("Access Granted")
+        break
+    if attempts >= 3:
+        print("Too many attempts. Try later.")
+        break
