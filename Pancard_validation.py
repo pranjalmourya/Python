31daaed07e3acbf340dcf14f001f2983e3cf747c
pranjalmourya <pranjalmaurya278@gmail.com>
2026-04-17 20:07:04 +0530

    not text

---
+++ Pancard_validation.py
@@ -0,0 +1,10 @@
+# import pandas as pd
+# df = pd.read_excel(r"C:\Users\ad\Downloads\PAN_Card_Validation_in_PYTHON-Scripts\PAN_Number_Validation_Dataset.xlsx")
+# print(len(df))
+# df['Pan_Numbers'] = df['Pan_Numbers'].astype('string').str.strip().str.upper()
+# # print(len(df))
+# df = df.replace({'Pan_Numbers':''},pd.NA)
+# print(df['Pan_Numbers'].dropna().drop_duplicates())
+# print(df.count().sum())
+
+print(190627/228)
